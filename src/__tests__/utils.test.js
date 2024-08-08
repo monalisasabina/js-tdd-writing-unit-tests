8520c4ccace8445d79e5cf86ff62c0074b97d92b
Monalisa Onyango <+>
2024-08-08 19:56:01 +0300

    test lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+
+// describe("pointsForWord", () =>{
+//    it("calculates the total points for a word(1 point per vowel, 2 per consonant)",() =>{
+     
+//      const word="test";
+
+//      const points= pointsForWord(word);
+
+//      expect(points).toBe(7);
+
+//    });
+// });
+
+
+describe("pointsForWord", () =>{
+    it("handles uppercase and lowercase input",() =>{
+      
+      const word="tEsT";
+ 
+      const points= pointsForWord(word);
+ 
+      expect(points).toBe(7);
+ 
+    });
+ });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,30 @@
 // Your code here
+
+export function pointsForWord(word) {
+    // let points = 0;
+    // for (const char of word) {
+    //   if (["a", "e", "i", "o", "u"].includes(char)) {
+    //     points += 1;
+    //   } else {
+    //     points += 2;
+    //   }
+    // }
+    // return points;
+
+   
+    // Refactor
+    // let points = 0;
+    // for (const char of word) {
+    //   points += /[aeiou]/.test(char) ? 1 : 2;
+    // }
+    // return points;
+
+    
+    let points = 0;
+    for (const char of word) {
+    points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+      
+
+  }
